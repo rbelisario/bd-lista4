9d88096484feed0ddb3da4a64ac0364e70cde92a
Mateus Santos <mssantos456@gmail.com>
2021-10-13 18:51:13 -0300

    4c

--- Lista4Ex4.sql
+++ Lista4Ex4.sql
@@ -41,3 +41,27 @@ delete from equipe where idequipe = 1;
  tabela JOGA: jogos que a equipe removida tenha participado também são removidos
  tabela PARTIDA: partidas que a equipe removida tenha participado tambpem são removidas
  */
+
+
+ALTER TABLE "UNIFORME" DROP CONSTRAINT "uniforme_fk";
+ALTER TABLE "UNIFORME" ADD CONSTRAINT "uniforme_fk" FOREIGN KEY ("IdEquipe")
+REFERENCES Equipe ("IdEquipe") ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE "DIRETOR" DROP CONSTRAINT "diretor_fk";
+ALTER TABLE "DIRETOR" ADD CONSTRAINT "diretor_fk" FOREIGN KEY ("IdEquipe")
+REFERENCES Equipe ("IdEquipe") ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE "JOGADOR" DROP CONSTRAINT "jogador_fk";
+ALTER TABLE "JOGADOR" ADD CONSTRAINT "jogador_fk" FOREIGN KEY ("IdEquipe")
+REFERENCES Equipe ("IdEquipe") ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE "Partida" DROP CONSTRAINT "partida_fk";
+ALTER TABLE "Partida" ADD CONSTRAINT "partida_fk" FOREIGN KEY ("IdEquipe1, IdEquipe2")
+REFERENCES Joga ("IdEquipe1, IdEquipe2") ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE "JOGA" DROP CONSTRAINT "joga_fk1";
+ALTER TABLE "JOGA" DROP CONSTRAINT "joga_fk2";
+ALTER TABLE "JOGA" ADD CONSTRAINT "joga_fk1" FOREIGN KEY ("IdEquipe")
+REFERENCES Equipe ("IdEquipe") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "JOGA" ADD CONSTRAINT "joga_fk2" FOREIGN KEY ("IdEquipe")
+REFERENCES Equipe ("IdEquipe") ON DELETE CASCADE ON UPDATE CASCADE;
